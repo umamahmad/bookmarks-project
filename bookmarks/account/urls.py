7575dbb4e6e9f9f84b73c3831d9adda47eee2f71
Umam Ahmad <umamahmad2006@gmail.com>
2026-05-02 16:24:10 +0530

    added login logout in urls

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,6 +1,11 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    #previous login url
+    #path('login/', views.user_login, name='login'),
+    #login/logout
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
